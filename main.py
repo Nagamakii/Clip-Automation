f37378863e1dde96f4245a2584c759e293c51799
Evan Quah <evanjquah@gmail.com>
2022-01-02 23:24:01 -0600

    Changed a couple things

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import requests
 from requests import api
-import discord
 import os
 from dotenv import load_dotenv
+import moviepy.editor
+import discord
 
 # Get enviornmental variables and establish connection with discord
 load_dotenv()
+user_file = input("File: ")
+user_file_stripped = user_file.strip('"')
 client = discord.Client()
 
 # Secret info
@@ -13,33 +16,39 @@ api = os.getenv('API')
 token = os.getenv('TOKEN')
 
 # Prep data for POST requests
-url = 'https://upload.giphy.com/v1/gifs'
-api_key = {'api_key': api}
-file = {'file': open('Test.mp4','rb')}
+giphy_url = 'https://upload.giphy.com/v1/gifs'
+api_key = {
+    'api_key': api
+    }
+file = {
+    'file': open(user_file_stripped,'rb')
+    }
 
-# Post clip to GIPHY, and get link from JSON response
-def upload():
-    authorize = requests.post(url, data=api_key, files=file)
-    print(authorize.status_code)
-    format_data = authorize.json()
-
-    id = format_data['data']
-
-    url_id = id['id']
-
-    gif_url = f'https://giphy.com/gifs/{url_id}'
+# Post clip to GIPHY, format response to get link, and send in discord
+def main():
+    video = moviepy.editor.VideoFileClip(user_file_stripped)
+    vid_len = int(video.duration)
+    if vid_len > 15:
+        print("GIPHY only allows for 15sec clips, please clip to under 15 until a better solution is found.")
+        quit()
+    else:
+        print('Clip is good!')
+        print('Uploading and Encoding...')
+        gif_upload = requests.post(
+            giphy_url, data=api_key, files=file
+            )
+        print(gif_upload.status_code, gif_upload.text)
+        
+        format_data = gif_upload.json()
+        id = format_data['data']
+        url_id = id['id']
+        gif_url = f'https://giphy.com/gifs/{url_id}'
     
-    return gif_url
-
-# idk man this looks dumb but whatever it works
-discord_url = upload()
-print(discord_url)
-
-# Post link to discord
-@client.event
-async def on_ready():
-    await client.get_channel(869809254498979945).send(discord_url)
-client.run(token)
-
+        print('Posting Message...')
+        @client.event
+        async def on_ready():
+            await client.get_channel(815836813221822475).send(gif_url)
+        client.run(token)
+        
 if __name__ == "__main__":
-    upload()
+    main()
