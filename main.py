c6f8c20c86a615ed58fa2c75901100d54dda970f
Evan Quah <evanjquah@gmail.com>
2022-01-02 20:28:39 -0600

    Added comments

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import requests
 from requests import api
-from requests.api import post
-from requests.models import Response
 import discord
 import os
 from dotenv import load_dotenv
 
+# Get enviornmental variables and establish connection with discord
 load_dotenv()
 client = discord.Client()
 
+# Secret info
 api = os.getenv('API')
 token = os.getenv('TOKEN')
 
+# Prep data for POST requests
 url = 'https://upload.giphy.com/v1/gifs'
 api_key = {'api_key': api}
 file = {'file': open('Test.mp4','rb')}
 
+# Post clip to GIPHY, and get link from JSON response
 def upload():
-    
     authorize = requests.post(url, data=api_key, files=file)
     print(authorize.status_code)
     format_data = authorize.json()
@@ -30,9 +31,11 @@ def upload():
     
     return gif_url
 
+# idk man this looks dumb but whatever it works
 discord_url = upload()
 print(discord_url)
 
+# Post link to discord
 @client.event
 async def on_ready():
     await client.get_channel(869809254498979945).send(discord_url)
