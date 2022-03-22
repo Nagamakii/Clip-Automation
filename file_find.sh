d42dad5a913463a81e05a1ad308abba68477c76e
Evan Quah <evanjquah@gmail.com>
2022-03-22 14:54:29 -0500

    Removing some no longer needed files

--- file_find.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DIR="/var/www/lager-scanner/filer/Nav/FromNav"
-inotifywait -m -r -e create "$DIR" | while read f
-
-do
-    # you may want to release the monkey after the test :)
-    echo monkey
-    main.py
-done
